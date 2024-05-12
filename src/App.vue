3ffee85df3e5695606322eed37804ab8df15f623
mardbrito <marcelo4web@gmail.com>
2024-05-12 10:47:15 -0400

    Add PostCreate and your route

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/post-create">New Post</RouterLink>
     </nav>
   </header>
 
---
+++ src/views/PostCreate.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { reactive, computed } from 'vue'
+
+const post = reactive({
+  title: 'my title',
+  body: ''
+})
+
+const isFormInvalid = computed(() => {
+  return post.title === '' || post.body === ''
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <form>
+      <h3>Create a new post</h3>
+      <div>
+        <label>Post title</label>
+        <input type="text" v-model="post.title" />
+      </div>
+      <div>
+        <label>Post body</label>
+        <textarea rows="7" v-model="post.body"></textarea>
+      </div>
+      <div>
+        <button :disabled="isFormInvalid">Add</button>
+      </div>
+    </form>
+  </div>
+</template>
