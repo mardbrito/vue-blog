40b9daae845368b87df268e7244139bc7c555bb2
mardbrito <marcelo4web@gmail.com>
2024-05-11 20:23:15 -0400

    Add PostItem component

---
+++ src/components/PostItem.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <div class="header">
+      <span>Written by: {{ post.author }} on {{ post.created_at }}</span>
+      <div>
+        <button class="del">delete</button>
+        <button class="save">bookmark</button>
+      </div>
+    </div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,45 @@
-<script setup></script>
+<script setup>
+import PostItem from '@/components/PostItem.vue'
+const posts = [
+  {
+    id: 1,
+    title: 'This great book!',
+    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+    author: 'Sarah Doe',
+    created_at: '11/06/2023',
+    is_saved: false
+  },
+  {
+    id: 2,
+    title: 'Coding is fun!?',
+    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+    author: 'Jon Doe',
+    created_at: '06/04/2023',
+    is_saved: false
+  },
+  {
+    id: 3,
+    title: 'Vue js VS React',
+    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+    author: 'Jon Doe',
+    created_at: '12/30/2022',
+    is_saved: false
+  },
+  {
+    id: 4,
+    title: 'Video games',
+    body: 'tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit',
+    author: 'Jon Doe',
+    created_at: '05/01/2023',
+    is_saved: false
+  }
+]
+</script>
 
 <template>
-  <main>Home</main>
+  <div v-for="post in posts" :key="post.id">
+    <div class="wrapper">
+      <PostItem :post="post" />
+    </div>
+  </div>
 </template>
